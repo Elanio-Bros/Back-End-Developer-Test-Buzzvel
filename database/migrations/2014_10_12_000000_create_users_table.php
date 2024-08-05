4074e8e60693c7dacebbf143f0d54f321b4b1264
unknown <elaniobros@gmail.com>
2024-08-05 09:34:54 -0300

    configure docker-compose and create migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->tinyInteger('status', false)->default(1);
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamp('update_at');
+            $table->timestamp('created_at')->useCurrent();
         });
     }
 
--- database/migrations/2024_07_30_142302_create_holiday_plans_table.php
+++ database/migrations/2024_07_30_142302_create_holiday_plans_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('holiday_plans', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('title')->nullable(false);
+            $table->text('description')->nullable(false);
+            $table->timestamp('date')->useCurrent();
+            $table->json('participants');
         });
     }
 
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('products');
     }
 };
