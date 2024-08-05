b7aa517a340f87904277eae50ff30d847eac84f4
Elânio Leal Alves (Bros) <elaniobros@gmail.com>
2024-08-05 07:14:52 -0700

    add jwt auth and fix migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->tinyInteger('status', false)->default(1);
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
--- database/migrations/2024_07_30_142302_create_holiday_plans_table.php
+++ database/migrations/2024_07_30_142302_create_holiday_plans_table.php
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('holiday_plans');
     }
 };
